10200b765ee2aaf74f0dcd2d012814e81b5ef3e3
Roland Ernst <rcrernst@gmail.com>
2019-08-22 20:39:37 +0200

    Extend tests to 'VideoGrabber.grabInetref'.
    These tests belong to the use case shown in 'mythvidexport.py'.

--- test/test_Dataheap_VideoGrabber_001.py
+++ test/test_Dataheap_VideoGrabber_001.py
@@ -7,7 +7,7 @@ from lxml import etree
 
 #from MythTV import MythDB, RecordedArtwork, Video, VideoGrabber
 
-from helpers import get_test_env, add_log_flags
+from test.helpers import get_test_env, add_log_flags
 
 # globals:
 TestEnv = {}
@@ -44,7 +44,6 @@ def restore_static_prefix(rewrt_file, org_file):
         return(False)
 
 
-
 def setUpModule():
     """called once, before anything else in this module"""
     # pull in test environment as dict
@@ -78,9 +77,8 @@ def tearDownModule():
     restore_static_prefix(rewrt_file, org_file)
 
 
-
-class test_Dataheap_Video_001(unittest.TestCase):
-    """Test class 'Videos' from dataheap.
+class test_Dataheap_VideoGrabber_001(unittest.TestCase):
+    """Test class 'VideoGrabber' from dataheap.
     """
 
     @classmethod
@@ -154,7 +152,7 @@ class test_Dataheap_Video_001(unittest.TestCase):
                 inetref = inetrefstr
 
             vids = self.mydb.searchVideos( title = title )
-            vid = vids.next()
+            vid = next(vids)
             # print("%s : %s" %(vid.title, type(vid.title)))
             self.assertTrue(isinstance(vid, Video))
             grab = VideoGrabber("Movie", lang = lang, db = self.mydb)
@@ -206,7 +204,7 @@ class test_Dataheap_Video_001(unittest.TestCase):
                                      , encoding        = "UTF-8"
                                      , standalone      = "yes"
                                      )
-            xml_file  = open("/tmp/details.xml", "w")
+            xml_file  = open("/tmp/details.xml", "wb")
             xml_file.write(xml_str)
             xml_file.close()
 
@@ -218,6 +216,111 @@ class test_Dataheap_Video_001(unittest.TestCase):
                     cast_found = True
             self.assertTrue(cast_found)
 
+
+class test_Dataheap_VideoGrabber_002(unittest.TestCase):
+    """Test class 'VideoGrabber' from dataheap.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        # get the global test environment
+        global TestEnv
+        cls.testenv = TestEnv
+        # moke INSTALL_PREFIX
+        global INSTALL_PREFIX
+        cls.INSTALL_PREFIX = INSTALL_PREFIX
+        # clear 'pytmdb3.cache'
+        pytmdb3_cache = os.path.expanduser('~/.mythtv/cache/pytmdb3.cache')
+        if os.path.exists(pytmdb3_cache):
+            os.remove(pytmdb3_cache)
+
+    def setUp(self):
+        # check INSTALL_PREFIX
+        if (self.INSTALL_PREFIX != os.getcwd()):
+            self.fail("Unable to rewrite 'INSTALL_PREFIX' in static.py!")
+
+    def tearDown(self):
+        if os.path.exists("/tmp/my_logfile"):
+            os.remove("/tmp/my_logfile")
+        if os.path.exists("/tmp/details.xml"):
+            os.remove("/tmp/details.xml")
+
+    def is_sublist(self, list1, list2):
+       ls1 = [element for element in list1 if element in list2]
+       ls2 = [element for element in list2 if element in list1]
+       return ls1 == ls2
+
+
+    def test_Dataheap_VideoGrabber_002_importMetadata_01(self):
+        """Test 'VideoGrabber.grabInetref' method and 'Video.importMetadata'
+           methods using predefined values. See 'mythvidexport.py' as well.
+           See MythtTV trac tickets #12243 and #12245.
+           Note: Use a video with French accents in title and cast.
+        """
+        from MythTV import MythDB, Video, VideoGrabber
+
+        with add_log_flags():
+
+            title      = u"Le Dernier Métro"
+            castlist   = [u"Catherine Deneuve", u"Gérard Depardieu", u"Andréa Ferréol"]
+            inetstr    = "tmdb3.py_1716"
+            lang       = "fr"
+            filename   = title + u".mkv"
+
+            # create a new Video instance
+            self.mydb = MythDB()
+            vid  = Video(db=self.mydb).create ({'title'   : title,
+                                                'filename': filename,
+                                                'host'    : self.mydb.getMasterBackend()})
+            # grab details from www.themoviedb.org
+            grab = VideoGrabber('MOVIE', lang=lang, db=self.mydb)
+            metadata = grab.grabInetref(inetstr)
+            #print(type(metadata))   -->   <class 'MythTV.system.VideoMetadata'>
+            # import metadata into Video instance
+            vid.importMetadata(metadata)
+            #print(vid.cast[1].cast)   ### --->  Gérard Depardieu
+            # create a list of cast members from metadata
+            vidcastmembers = [c.cast for c in vid.cast]
+            # check for members in cast list
+            self.assertTrue(self.is_sublist(castlist, vidcastmembers))
+            # delete previously created Video object in database
+            vid.delete()
+            #print(metadata.inetref)   # --> 1716
+            self.assertEqual(metadata.inetref, inetstr.split('_')[-1])
+            # run the grabber again, but based on a 'VideoMetadata' object
+            new_metadata = grab.grabInetref(metadata)
+            # check if collected metadata are the equal
+            self.assertEqual(new_metadata, metadata)
+
+    def test_Dataheap_VideoGrabber_002_importMetadata_02(self):
+        """Test 'VideoGrabber.grabInetref' method and 'Video.importMetadata'
+           methods using predefined values. See 'mythvidexport.py' as well.
+           See MythtTV trac tickets #12243 and #12245.
+           Note: Use a TV series with season and episode.
+        """
+        from MythTV import MythDB, Video, VideoGrabber
+
+        with add_log_flags():
+            self.mydb =  MythDB()
+
+            alf_metadata = { u'title'    : u'Alf',
+                             u'subtitle' : u'Wedding Bell Blues',
+                             u'season'   : 2,
+                             u'episode'  : 4,
+                             u'inetref'  : '78020'}
+
+            grab = VideoGrabber('TV', lang='en', db=self.mydb)
+            metadata = grab.grabInetref( alf_metadata['inetref'],
+                                         alf_metadata['season'],
+                                         alf_metadata['episode'])
+
+            # print(metadata.collectionref)   # ---> '78020'
+            self.assertEqual(metadata.collectionref, alf_metadata[u'inetref'])
+
+            # run the grabber again, but based on a 'VideoMetadata' object
+            new_metadata = grab.grabInetref(metadata)
+            # check if collected metadata are the equal
+            self.assertEqual(new_metadata, metadata)
 
 if __name__ == '__main__':
     unittest.main()
